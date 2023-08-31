75ece8657dba621c2272ead16927c20418c0e03c
matt-henrii <mathmarkub@gmai.com>
2023-08-31 20:28:07 -0300

    estudo-concat

--- java-anatomia-de-classes/lib/MinhaClasse.java
+++ java-anatomia-de-classes/lib/MinhaClasse.java
@@ -1,8 +1,15 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
+        String primeiroNome = "Mateus";
+        String segundoNome = "Henrique";
 
-        System.out.println("Olá, sejam bem vindos ao meu código!");
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
 
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método é: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
