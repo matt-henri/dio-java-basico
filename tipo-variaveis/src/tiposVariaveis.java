f282a5c71d0eda83e3b2bec7c674d24223f861a5
matt-henrii <mathmarkub@gmai.com>
2023-08-31 21:24:29 -0300

    tipo-de-dados

--- tipo-variaveis/src/tiposVariaveis.java
+++ tipo-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("!");
+        // Tipo de dados
+
+        System.out.println("Tipo de dados");
+        byte idade = 123;
+        short ano = 2023;
+        int cep = 38408525;
+        long cpf = 12712712701L;
+        float pi = 3.14F;
+        double salario = 1320.00;
+
     }
 }
